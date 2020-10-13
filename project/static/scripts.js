4dd43edb4c0981df2c2b9a979d2b0e1a7f70d915
Joseph <jxp2268@rit.edu>
2020-10-13 18:25:57 -0400

    updated javascript

--- project/static/scripts.js
+++ project/static/scripts.js
@@ -19,8 +19,14 @@ $(document).ready(function() {
     });
 
     // create button-specific callbacks for driving the tank
-    $('#left').bind('mousedown', function() { $.get($SCRIPT_ROOT + '/_left_start'); });
-    $('#left').bind('mouseup', function() { $.get($SCRIPT_ROOT + '/_left_stop'); });
+    document.getElementById("left_button").bind('mousedown', function() {
+                                                                    alert("Left mousedown event!");
+                                                                    $.get($SCRIPT_ROOT + '/_left_start');
+                                                                  });
+    document.getElementById("left_button").bind('mouseup', function() {
+                                                                    alert("Left mouseup event!");
+                                                                    $.get($SCRIPT_ROOT + '/_left_stop');
+                                                                  });
     $('a#right').bind('mousedown', function() { $.get($SCRIPT_ROOT + '/_right'); });
     $('a#forward').bind('mousedown', function() { $.getJSON($SCRIPT_ROOT + '/_forward', { forward: True }); });
     $('a#reverse').bind('mousedown', function() { $.getJSON($SCRIPT_ROOT + '/_reverse', { reverse: True }); });
