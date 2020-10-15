0de06633ce7ae0a3c2cede2a1d69a77ad052ce1f
Joseph <jxp2268@rit.edu>
2020-10-14 20:32:00 -0400

    updates

--- project/application.py
+++ project/application.py
@@ -39,7 +39,7 @@ def create_app(DEV: bool = True):
     # app.config["SESSION_FILE_DIR"] = mkdtemp()
     # app.config["SESSION_PERMANENT"] = False
     # app.config["SESSION_TYPE"] = "filesystem"
-
+    session.clear()
     # Ensure responses aren't cached
     @app.after_request
     def after_request(response):
