48b12b0e7039f62aff0797a0e2084242dd3ef0b8
Joseph <jxp2268@rit.edu>
2020-10-16 10:26:39 -0400

    updated to work better with phones

--- project/static/scripts.js
+++ project/static/scripts.js
@@ -6,21 +6,30 @@ $(document).ready(function() {
     $('a#left').bind('mousedown', function() { $.getJSON('/left_start', function(data) { /* do nothing */}); return false; });
     $('a#left').bind('mouseup', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
     $('a#left').bind('click', function(event) { event.preventDefault(); return false; });
+    $('a#left').bind('touchstart', function() { $.getJSON('/left_start', function(data) { /* do nothing */}); return false; });
+    $('a#left').bind('touchend', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
 
     $('a#right').bind('mousedown', function() { $.getJSON('/right_start', function(data) { /* do nothing */}); return false; });
     $('a#right').bind('mouseup', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
     $('a#right').bind('click', function(event) { event.preventDefault(); return false; });
+    $('a#right').bind('touchstart', function() { $.getJSON('/right_start', function(data) { /* do nothing */}); return false; });
+    $('a#right').bind('touchend', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
 
     $('a#forward').bind('mousedown', function() { $.getJSON('/forward_start', function(data) { /* do nothing */}); return false; });
     $('a#forward').bind('mouseup', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
     $('a#forward').bind('click', function(event) { event.preventDefault(); return false; });
+    $('a#forward').bind('touchstart', function() { $.getJSON('/forward_start', function(data) { /* do nothing */}); return false; });
+    $('a#forward').bind('touchend', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
 
     $('a#reverse').bind('mousedown', function() { $.getJSON('/reverse_start', function(data) { /* do nothing */}); return false; });
     $('a#reverse').bind('mouseup', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
     $('a#reverse').bind('click', function(event) { event.preventDefault(); return false; });
+    $('a#reverse').bind('touchstart', function() { $.getJSON('/reverse_start', function(data) { /* do nothing */}); return false; });
+    $('a#reverse').bind('touchend', function() { $.getJSON('/stop', function(data) { /* do nothing */}); return false; });
 
     $('a#shot').bind('mousedown', function() { $.getJSON('/shot_start', function(data) { /* do nothing */}); return false; });
     $('a#shot').bind('click', function(event) { event.preventDefault(); return false; });
+    $('a#shot').bind('touchstart', function() { $.getJSON('/shot_start', function(data) { /* do nothing */}); return false; });
 
     // $('#week_menu').on('change', function(x) {
     //
