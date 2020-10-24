b2bda2c52b6d574f99a2d55869bd39bf3b32ccec
Joseph <jxp2268@rit.edu>
2020-10-24 10:19:22 -0400

    removed thread from camera taking

--- project/application.py
+++ project/application.py
@@ -545,9 +545,9 @@ def create_app(DEV: bool = True, wait_timeout: int = 60, drive_timeout: int = 60
             assert next_user_IP == IP_addr
             print("User {} took a picture!".format(user_name))
             if not DEV:
-                count = len(glob.glob1("/home/pi","*.jpg"))
-                t = threading.Thread(target=os.system('uvccapture -v -m -x1280 -y960 -osnap{}.jpg'.format(count)), args=())
-                t.start()
+                count = len(glob.glob1("/home/pi/","*.jpg"))
+                t = os.system('uvccapture -v -m -x1280 -y960 -osnap{}.jpg'.format(count))
+                
 
         except:
             print("non-registered user has requested to take picture")
